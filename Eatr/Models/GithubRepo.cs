e85dc86bd762fc628b7f2009dfb94577dfdd4037
Frank Ngo <frankngomusic@gmail.com>
2018-05-16 11:06:05 -0700

    api call working

--- Eatr/Models/GithubRepo.cs
+++ Eatr/Models/GithubRepo.cs
@@ -14,47 +14,30 @@ namespace Eatr.Models
     public class YelpApi
     {
         public string name { get; set; }
-
+        public string image_url { get; set; }
+        public string phone { get; set; }
+        public string price { get; set; }
 
         public static YelpApi Recommend()
         {
-            //RestClient client = new RestClient("https://api.yelp.com");
-            //RestRequest request = new RestRequest("v3/businesses/north-india-restaurant-san-francisco", Method.GET);
-            //request.AddParameter("MaxResults", "40");
-            //request.AddParameter("OpenNow", "true");
 
 
-            //RestResponse response = new RestResponse();
-
-            //Task.Run(async () =>
-            //{
-            //    response = await GetResponseContentAsync(client, request) as RestResponse;
-            //}).Wait();
-
-            //JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
-            //return JsonConvert.DeserializeObject<List<YelpApi>>(jsonResponse["test"].ToString());
-
-
-
-  var client = new RestClient();
+            var client = new RestClient();
             client.BaseUrl = new Uri("https://api.yelp.com");
-      
+
 
             var request = new RestRequest();
-            request.AddHeader("header", "Authorization: Bearer _l4M0A_gWgknLXhScqC6gzMQMrsvyU9xLCa3siFYWD5qGD6Rkr1-x23V5nch_nLIz5ok5Xxyr5J0XKqAurr_0rHMcHzGkJ6SheksKfH2K5_QT54cVkfbxCZ8EXLzWnYx");
-
-            request.Resource = "v3/businesses/north-india-restaurant-san-francisco";
+            request.AddHeader("Authorization","Bearer _l4M0A_gWgknLXhScqC6gzMQMrsvyU9xLCa3siFYWD5qGD6Rkr1-x23V5nch_nLIz5ok5Xxyr5J0XKqAurr_0rHMcHzGkJ6SheksKfH2K5_QT54cVkfbxCZ8EXLzWnYx");
+            request.Resource = "/v3/businesses/north-india-restaurant-san-francisco";
 
             var response = new RestResponse();
             Task.Run(async () =>
             {
                 response = await GetResponseContentAsync(client, request) as RestResponse;
             }).Wait();
-           
-
             JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
-            var repo = JsonConvert.DeserializeObject<YelpApi>(jsonResponse.ToString());
-            return repo;
+            var repoList = JsonConvert.DeserializeObject<YelpApi>(jsonResponse.ToString());
+            return repoList;
 
         }
 
@@ -66,5 +49,6 @@ namespace Eatr.Models
             });
             return tcs.Task;
         }
+
     }
 }
